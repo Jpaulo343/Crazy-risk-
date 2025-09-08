5d16e96fd108ce06aeb55ec1199dc5c66f5ceaed
Jean Paulo Poveda Chaves <J.poveda.1@estudiantec.cr>
2025-09-07 21:26:27 -0600

    Se añadieron clases y paginas de interfaz
    Se añadió la clase Nodo, ListaEnlazada, ListaTerritorios, Territorios, y "pages" de wpf para el mapa del juegoy menú principal

---
+++ Crazy risk/Crazy risk/ListaEnlazada.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Crazy_risk
+{
+    public class Nodo<T>
+    {
+        public T Value { get; private set; }
+        public Nodo<T> Next { get; set; }
+
+        public Nodo(T dato)
+        {
+            Value = dato;
+        }
+        
+    }
+    public class ListaEnlazada<T>
+    {
+        protected Nodo<T> Head { get; set; }
+        protected Nodo<T> Tail { get; set; }
+
+        protected int size { get; set; }
+
+        public void añadir(T dato)
+        {
+            Nodo<T> nodo = new Nodo<T>(dato);
+            size++;
+            if (Head == null)
+            {
+                Tail = Head = nodo;
+                return;
+            }
+            else
+            {
+                Nodo<T> Temp = Head;
+                Head = nodo;
+                Head.Next = Temp;
+                return;
+            }
+        }
+
+        public bool buscar(T dato)
+        {
+
+            Nodo<T> actual = Head;
+            while (actual != null)
+            {
+                if (actual.Value.Equals(dato))
+                { 
+                    return true;
+                }
+
+                actual = actual.Next;
+            }
+            return false;
+        }
+
+    }
+
+    internal class ListaTerritorios : ListaEnlazada<Territorio> 
+    {
+        public  Territorio BuscarPorNombre(string nombre)
+        {
+            Nodo<Territorio> Temp = this.Head;
+            while (Temp != null) 
+            {   
+                if (Temp.Value.Nombre==nombre)
+                {
+                    return Temp.Value;
+                }
+                Temp = Temp.Next;
+            }
+            return null;
+        }
+    }
+
+}
+
---
+++ Crazy risk/Crazy risk/Territorio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crazy_risk
+{
+    internal class Territorio
+    {
+
+        internal string Nombre { get;  set; }
+        internal int Continente { get; private set; }
+        /*
+        0: norteamerica
+        1: sudamerica
+        2: europa
+        3: asia
+        4: africa
+        5: oceanía
+        */
+
+        internal string Estado { get; set; } // Puede ser conquistado por "x" jugdor o libre
+        internal int Tropas { get; set; }
+        internal ListaEnlazada<String> Adyacentes { get; set; }
+        public Territorio(string nombre, string estado, ListaEnlazada<String> adyacentes,int tropas, int continente)
+        {
+            Nombre=nombre;
+            Estado=estado;
+            Adyacentes=adyacentes;
+            Tropas=Tropas;
+            Continente = continente;
+        }
+
+
+    }
+}
